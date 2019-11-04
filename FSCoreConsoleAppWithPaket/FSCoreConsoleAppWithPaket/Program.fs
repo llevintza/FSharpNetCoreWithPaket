585e32f3048ffb0055b5bcc2cbe72633cec7042c
Leonte Levinta <leonte@ireckonu.com>
2019-11-04 14:40:24 +0100

    added the retrieval of the env variable read

--- FSCoreConsoleAppWithPaket/FSCoreConsoleAppWithPaket/Program.fs
+++ FSCoreConsoleAppWithPaket/FSCoreConsoleAppWithPaket/Program.fs
@@ -2,7 +2,17 @@
 
 open System
 
+let consulEnvironment = 
+    Environment.GetEnvironmentVariables()
+    |> Seq.cast<System.Collections.DictionaryEntry>
+    |> Seq.map (fun d -> d.Key :?> string, d.Value :?> string)
+    |> Seq.where (fun (k,_) -> k.StartsWith("CONSUL_"))
+    |> dict
+
 [<EntryPoint>]
 let main argv =
+    
+    for configurationKey in consulEnvironment do
+        printfn "Key %A Value %A" configurationKey.Key configurationKey.Value
     printfn "Hello World from F#!"
     0 // return an integer exit code
