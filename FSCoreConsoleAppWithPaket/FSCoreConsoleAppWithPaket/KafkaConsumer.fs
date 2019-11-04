2d0aaa6c3641c2619538c64afc39827438f4a150
Leonte Levinta <leonte@ireckonu.com>
2019-11-04 11:52:15 +0100

    added the default kafka producer-consumer

---
+++ FSCoreConsoleAppWithPaket/FSCoreConsoleAppWithPaket/KafkaConsumer.fs
@@ -0,0 +1,22 @@
+﻿module KafkaConsumer
+
+open FSharp.Control
+open Kafunk
+open System
+
+let conn = Kafka.connHost "existential-host"
+
+/// Configuration.
+let consumerConfig = 
+  ConsumerConfig.create (
+
+    /// The name of the consumer group.
+    groupId = "consumer-group", 
+
+    /// The topic to consume.
+    topic = "absurd-topic")
+
+
+/// This creates a consumer and joins it to the group.
+let consumer =
+  Consumer.create conn consumerConfig
---
+++ FSCoreConsoleAppWithPaket/FSCoreConsoleAppWithPaket/KafkaProducer.fs
@@ -0,0 +1,48 @@
+﻿module KafkaProducer
+
+open Kafunk
+open System
+
+let conn = Kafka.connHost "existential-host"
+
+/// Configuration.
+let producerConfig = 
+  ProducerConfig.create (
+    
+    /// The topic to produce to.
+    topic = "absurd-topic", 
+
+    /// The partition function to use.
+    partition = Partitioner.roundRobin,
+
+    /// The required acks setting.
+    requiredAcks = RequiredAcks.AllInSync,
+
+    /// The per-broker in-memory buffer size, in bytes.
+    bufferSizeBytes = ProducerConfig.DefaultBufferSizeBytes,
+
+    /// The maximum size, in bytes, of an individual produce request.
+    batchSizeBytes = ProducerConfig.DefaultBatchSizeBytes,
+    
+    /// The maximum time to wait for a batch.
+    batchLingerMs = ProducerConfig.DefaultBatchLingerMs)
+
+
+/// Create a producer.
+let producer = 
+  Producer.create conn producerConfig
+
+
+/// Create a message.
+let m = 
+  ProducerMessage.ofString (
+    value = "hello value", 
+    key = "hello key")
+
+
+/// Produce a single message.
+let prodRes = 
+  Producer.produce producer m 
+  |> Async.RunSynchronously
+
+printfn "partition=%i offset=%i" prodRes.partition prodRes.offset
