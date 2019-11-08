814c0f5308069da090cbe393bfd7f6a9356f7da9
Leonte Levinta <llevintza@gmail.com>
2019-11-08 18:13:19 +0100

    moved ksfka config to separate module

---
+++ FSCoreConsoleAppWithPaket/FSCoreConsoleAppWithPaket/KafkaConfig.fs
@@ -0,0 +1,61 @@
+﻿module KafkaConfig
+
+open System
+open System.Text
+
+open FSharp.Data
+
+open Consul
+
+let consulKey = Environment.GetEnvironmentVariable "CONSUL_KAFKA_CONFIG_KEY"
+
+
+type KafkaConfiguration = JsonProvider<"""{
+"endpoint": "",
+"topics": [
+  {
+    "name": "main-topic",
+    "consumerGroups": [
+      "consumerGroup1",
+      "consumerGroup2"
+    ]
+  },
+  {
+    "name": "secondary-topic",
+    "consumerGroups": [
+      "consumerGroup1",
+      "consumerGroup2"
+    ]
+  }
+]
+}
+""">
+
+let loadKafkaConfigurations (key:string) = async {
+    printfn "[TRACE] - Trying to load consul configurations!"
+    use consulClient = new ConsulClient()
+    let! queryResult = 
+        consulClient.KV.Get(key) 
+        |> Async.AwaitTask
+
+    let responseResult =
+        match queryResult.StatusCode with
+        | Net.HttpStatusCode.OK -> 
+            match queryResult.Response with
+            | (kvPair) -> Some(KafkaConfiguration.Parse(Encoding.UTF8.GetString(kvPair.Value)))
+            | _ -> None
+        | _ -> None 
+
+    let logMessage = 
+        match responseResult with
+        | Some(config) -> sprintf "Successfully read the configurations from consul with value %A" config
+        | None -> sprintf "Failed to read consul key %s" key
+
+    printfn "[TRACE] - %s" logMessage
+
+    return responseResult
+}
+
+let loadConfig  = async {
+    return! loadKafkaConfigurations consulKey
+}
--- FSCoreConsoleAppWithPaket/FSCoreConsoleAppWithPaket/Program.fs
+++ FSCoreConsoleAppWithPaket/FSCoreConsoleAppWithPaket/Program.fs
@@ -1,44 +1,18 @@
-﻿open System
-open System.Text
-open Consul
+﻿//open System
+//open System.Text
+//open Consul
 
-let consulKey = Environment.GetEnvironmentVariable "CONSUL_KAFKA_CONFIG_KEY"
-
-let loadKafkaConfigurations (key:string) = async {
-    printfn "[TRACE] - Trying to load consul configurations!"
-    use consulClient = new ConsulClient()
-    let! queryResult = 
-        consulClient.KV.Get(key) 
-        |> Async.AwaitTask
-
-    let responseResult =
-        match queryResult.StatusCode with
-        | Net.HttpStatusCode.OK -> 
-            match queryResult.Response with
-            | (kvPair) -> Some(Encoding.UTF8.GetString(kvPair.Value))
-            | _ -> None
-        | _ -> None 
-
-    let logMessage = 
-        match responseResult with
-        | Some(x) -> sprintf "Successfully read the configurations from consul with value %s" x
-        | None -> sprintf "Failed to read consul key %s" key
-
-    printfn "[TRACE] - %s" logMessage
-
-    return responseResult
-}
 
 [<EntryPoint>]
 let main argv =
     
     let config = 
-        loadKafkaConfigurations consulKey
+        KafkaConfig.loadConfig
         |> Async.RunSynchronously
 
     let log = 
         match config with
-        | Some validConfig -> sprintf "Kafka configuration is %s" validConfig
+        | Some validConfig -> sprintf "Kafka configuration is %A" validConfig
         | None -> sprintf "Could not retrieve Kafka configurations from consul"
 
     printfn "[TRACE] - %s" log
